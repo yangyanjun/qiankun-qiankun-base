114fdac77ca29862107833be9a0028f5fe72c9b4
yangyanjun <375663014@qq.com>
2023-06-18 23:46:21 +0800

    base commit 1

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,16 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+  <div>
+    <el-menu :router="true" mode="horizontal">
+      <!-- 基座中可以放自己的路由 -->
+      <el-menu-item index="/">Home</el-menu-item>
+      <el-menu-item index="/vue">vue应用</el-menu-item>
+      <el-menu-item index="/react">react应用</el-menu-item>
+    </el-menu>
     <router-view/>
+    <div id="vue"></div>
+    <div id="react"></div>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,37 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
+import router from './router';
+Vue.use(ElementUI);
 
-Vue.config.productionTip = false
-
+import { registerMicroApps, start } from 'qiankun';
+const apps = [
+  {
+    name: 'vueApp', // 应用的名字
+    entry: '//localhost:10000', // 默认会加载这个html 解析里面的js 动态的执行（子应用必须支持跨域）fetch
+    container: '#vue', // 容器名
+    activeRule: '/vue', // 激活的路径
+    props: {
+      a: 1,
+      b: 2,
+    },
+  },
+  {
+    name: 'reactApp',
+    entry: '//localhost:20000', // 默认会加载这个html 解析里面的js 动态的执行（子应用必须支持跨域）fetch
+    container: '#react',
+    activeRule: '/react',
+    props: {
+      c: 3,
+      d: 4,
+    },
+  }
+]
+registerMicroApps(apps);
+start({
+  prefetch: false,
+});
 new Vue({
   router,
   render: h => h(App)
